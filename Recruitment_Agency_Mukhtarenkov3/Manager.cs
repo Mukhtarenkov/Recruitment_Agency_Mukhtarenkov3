dd6163ab320406ffcbd78f33c759c484a2b6b5bf
roton <roton@DESKTOP-E99LCEA>
2024-03-01 10:08:16 +1000

    Добавьте файлы проекта.

---
+++ Recruitment_Agency_Mukhtarenkov3/Manager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Recruitment_Agency_Mukhtarenkov3
+{
+    internal class Manager
+    {
+        public static Frame MyFrame;
+
+        public static User _currentUser;
+    }
+}
